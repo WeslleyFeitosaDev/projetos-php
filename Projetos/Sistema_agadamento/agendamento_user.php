c899e8bd0280dea19f8da508cb55ef8b274714eb
Weslley Feitosa Barbosa <weslleybarbosa662@gmail.com>
2025-05-14 22:22:08 -0300

    Initial commit

---
+++ Projetos/Sistema_agadamento/agendamento_user.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendamento Usuario</title>
+</head>
+<body>
+    <!-- mensagem de boas vindas -->
+    <div class="nome_paciente">
+        <h1>Seja bem vindo </h1>
+        <p>marque sua consuta</p>
+    </div>
+    <!-- os dias disponiveis -->
+
+    <!-- pegar a data deseja do user -->
+    <div class="dia_horario_consuta_user">
+        <form action="" method="post">
+            <label for="">Dia da consuta:</label>
+            <input type="date" name="saida_data_user" id="saida_data_user">
+            <label for="">Hora da consuta</label>
+            <input type="time" name="saida_hora_user" id="saida_hora_user">
+        </form>
+    </div>
+    <!-- mensagem de marcação da cosulta -->
+    <div class="mensgem_confiamarcao_consulta">
+        
+    </div>
+
+    <div class="infor_consultas">
+        <div class="infor_medico">
+            <!-- nome do medico -->
+            <p>Nome do médico: </p>
+            <p>Dia: </p>
+            <p>Horario:</p>
+
+            <!-- em que sala ele vai ser atendido -->
+            <p>sala: </p>
+        </div>
+    </div>
+    
+   
+    
+
+    
+</body>
+</html>
---
+++ Projetos/Sistema_agadamento/sistema_empresa.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema da empresa</title>
+</head>
+<body>
+    <!-- regras -->
+        <!-- não pode ter consuta com mesmo medico no mesmo dia na mesma hora -->
+        <!-- não pode ter um dia e uma hora marcada, depois que medico finalizar a consulta -->
+
+    <!-- o dia que o user escolheu a sua consuta-->
+    <!-- lançar dias disponiveis e desocupados -->
+    <!-- qual medico tá disponivel -->
+    <!-- salas disponiveis -->
+    <!-- quantidade de medicos livres -->
+</body>
+</html>
